60c953b22aca812e72dedde1d2357c3928024b7f
chenhao <chenhao@chenhaos-iMac.local>
2016-09-02 16:19:45 +0800

    product

--- server/api/product/index.js
+++ server/api/product/index.js
@@ -9,9 +9,12 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/count', controller.count);
 router.get('/:id', controller.show);
-router.post('/', auth.hasRole('admin'), controller.create);
+router.post('/',  controller.create);
 router.put('/:id', auth.hasRole('admin'), controller.update);
 router.patch('/:id', auth.hasRole('admin'), controller.update);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
+
+
+
 
 module.exports = router;
