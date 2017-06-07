c1542ee5d4ccb3afdb515bdecbd33c513e514dba
ybyGold <415098820@qq.com>
2017-06-06 22:20:50 -0500

    make it better

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
+//
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
